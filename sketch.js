6ca7f3182e7b9b9b966ae37726f9fc78243a1dcc
emilly233545 <125043113+emilly233545@users.noreply.github.com>
2025-11-11 11:09:17 -0300

    funções básicas

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
 function setup() {
-    createCanvas(400, 400);
-}
+    createCanvas(4000, 400);
+}   //define valores padrão do projeto//
 
 function draw() {
-    background(220);
-}
 
-  function setup() {
-    createCanvas(600, 400);
+    //circulo
+
+    background(135, 206, 235);
+    fill("yellow")  //define a cor de qualquer forma na tela//
+    stroke("orange")    //Define a cor usada para desenhar pontos, linhas e contornos de formas.//
+    strokeWeight(20)  //largura do traço//
+    circle(200, 50, 50);
+    
+     // Elipse
+
+     fill("white"); 
+     noStroke(); // sem contorno
+     ellipse(100, 100, 80, 50); 
 }
-function draw() {
-     background(220);
-}
+    
+
